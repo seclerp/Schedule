bf2e81cacb837f9931e4d90a9b243b2c7f9de949
Andrew Rublyov <craftmaniack@gmail.com>
2018-06-09 19:02:53 +0300

    Add home controller

---
+++ src/NURE.Schedule.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NURE.Schedule.Api.Models.Response;
+
+namespace NURE.Schedule.Api.Controllers
+{
+  [Route("")]
+  public class HomeController : Controller
+  {
+    public string Get()
+    {
+      return "NURE Schedule API is running";
+    }
+  }
+}
